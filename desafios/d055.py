c404910f364cdfff48f66ac7f60c4059a1e597f1
Isabelle Peres da Luz <isabelle_git@hotmail.com>
2024-02-10 15:02:37 -0300

    Create d055.py

---
+++ desafios/d055.py
@@ -0,0 +1,15 @@
+peso = 0
+maior = 0
+menor = 0
+
+for pess in range(1, 6):
+    peso = float(input(f'Peso da {pess}ª pessoa'))
+    if peso == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print(f'O maior peso foi {maior} e o menor peso foi {menor}')
