97a9a609bf24485c65aaac2af3e7d9b524e276be
Isabelle Peres da Luz <isabelle_git@hotmail.com>
2024-03-11 15:57:51 -0300

    d096.py & d097.py

---
+++ desafios/d096.py
@@ -0,0 +1,9 @@
+def area(l,c):
+    a = l * c
+    print(f'A área de um terreno {l:.1f}x{c:.1f} é de {a:.1f}m²')
+
+print('Controle de Terrenos')
+print('-' * 20)
+larg = float(input('LARGURA (m): '))
+comp = float(input('COMPRIMENTO (m): '))
+area(larg, comp)
---
+++ desafios/d097.py
@@ -0,0 +1,7 @@
+def write(text):
+    print('~' * (len(text)+4))
+    print(f'  {text} ')
+    print('~' * (len(text)+4))
+write('Isabelle Peres')
+write('Hello')
+write('Whats my name')
