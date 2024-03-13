edcfd5befcda8475b30fe218ee3104eef5ede586
Isabelle Peres da Luz <isabelle_git@hotmail.com>
2024-03-13 17:03:38 -0300

    Create d102.py

---
+++ desafios/d102.py
@@ -0,0 +1,12 @@
+def fatorial(num, show=False):
+    f = 1
+    for c in range(num, 0, -1):
+        if show==True:
+            print(c, end=' ')
+            if c > 1:
+                print( ' x ', end=' ')
+            else:
+                 print( ' = ', end=' ')
+        f *= c
+    return f
+print(fatorial(5, True))
