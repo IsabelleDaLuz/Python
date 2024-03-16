79838e7b45ba2b74ea123c5d4d1107eed7aefe89
Isabelle Peres da Luz <isabelle_git@hotmail.com>
2024-03-16 14:37:39 -0300

    Update d108.py

--- desafios/d108/d108.py
+++ desafios/d108/d108.py
@@ -1,5 +1,5 @@
-from d108 import moeda
-p = float(input('Digite o preço: R$'))
-#print(f'A metade de R${p} é R${moeda.metade(p)}')
-#print(f'O dobro de R${p} é R${moeda.dobro(p)}')
-#print(f'Aumentando 10%, temos {moeda.aumentar(p, 10)}')
+from moeda import dobro, metade, aumentar
+p = int(input('Digite o preço: R$'))
+print(f'A metade de R${p} é R${metade(p)}')
+print(f'O dobro de R${p} é R${dobro(p)}')
+print(f'Aumentando 10%, temos {aumentar(p, 10)}')
